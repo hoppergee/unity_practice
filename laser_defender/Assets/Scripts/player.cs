490efef35ac2f46c43328360fca60605086de544
Xiang Ji <jx4554@gmail.com>
2018-12-02 14:32:16 +0800

    Add laser to player

--- laser_defender/Assets/Scripts/player.cs
+++ laser_defender/Assets/Scripts/player.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
+  // configuraiton parameters
   [SerializeField] float moveSpeed = 10f;
   [SerializeField] float padding = 1f;
+  [SerializeField] GameObject laserPrefab;
+  [SerializeField] float projectileSpeed = 10f;
 
   float xMin;
   float xMax;
@@ -17,18 +20,21 @@ public class player : MonoBehaviour {
 	  SetUpMoveBoundaries();	
 	}
 
-  private void SetUpMoveBoundaries() {
-    Camera gameCamera = Camera.main;
-    xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
-    xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
-    yMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y + padding;
-    yMax = gameCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y - padding;
-  }
-	
 	// Update is called once per frame
 	void Update () {
     Move();
+    Fire();
 	}
+
+  private void Fire() {
+    if (Input.GetButtonDown("Fire1")) {
+      GameObject laser = Instantiate(
+          laserPrefab,
+          transform.position,
+          Quaternion.identity) as GameObject;
+      laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+    }
+  }
 
   private void Move() {
 	  var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
@@ -39,4 +45,13 @@ public class player : MonoBehaviour {
     
     transform.position = new Vector2(newXPos, newYPos);
   }
+
+  private void SetUpMoveBoundaries() {
+    Camera gameCamera = Camera.main;
+    xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+    xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+    yMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y + padding;
+    yMax = gameCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y - padding;
+  }
+	
 }
