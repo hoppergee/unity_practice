42e73c76873dc2041ee54732ef4bcfd294926e9e
Xiang Ji <jx4554@gmail.com>
2018-12-06 21:27:26 +0800

    Make enemy won't destroy enemy by config collision layer

--- laser_defender/Assets/Scripts/player.cs
+++ laser_defender/Assets/Scripts/player.cs
@@ -73,11 +73,13 @@ public class player : MonoBehaviour {
 
   private void OnTriggerEnter2D(Collider2D other) {
     DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+    if (!damageDealer) { return; }
     ProcessHit(damageDealer);
   }
 
   private void ProcessHit(DamageDealer damageDealer) {
     health -= damageDealer.GetDamage();
+    damageDealer.Hit();
     if (health <= 0) {
       Destroy(gameObject);
     }
